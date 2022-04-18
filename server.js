bfff2bad421095a6eff10dd890af07b145f6b34c
Tabu Hana <taboodog@hotmail.com>
2022-04-18 11:27:02 -0700

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const db = require('./config/connection.js')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(require('./routes'))
+
+// sync sequelize models to the database, then turn on the server
+db.sync()
+app.listen(process.env.PORT || 3000)
+
+//need to import express and mongoose
