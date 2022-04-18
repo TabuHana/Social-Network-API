c41c9ba21cfddb64c862d29ec845c66f7f3bf325
Tabu Hana <taboodog@hotmail.com>
2022-04-18 13:06:36 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-const router = require('express').Router()
+const express = require('express')
+const router = express()
 
 router.use('/api', require('./userRoutes.js'))
 router.use('/api', require('./thoughtRoutes.js'))
